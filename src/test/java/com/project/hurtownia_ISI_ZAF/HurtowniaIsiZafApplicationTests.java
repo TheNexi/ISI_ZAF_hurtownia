5f90fa44eb6164559bfe7b5408e630a997174e17
TheNexi <127994491+TheNexi@users.noreply.github.com>
2025-04-01 14:22:45 +0200

    init

---
+++ src/test/java/com/project/hurtownia_ISI_ZAF/HurtowniaIsiZafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hurtownia_ISI_ZAF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HurtowniaIsiZafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
