76e4bc9bb3c654999961d528c5ea8c776b5921d4
TheNexi <127994491+TheNexi@users.noreply.github.com>
2025-05-06 14:05:04 +0200

    Configuration of OracleDB

---
+++ src/main/java/com/example/hurtownia_ISI_ZAF/HurtowniaIsiZafApplication.java
@@ -0,0 +1,13 @@
+package com.example.hurtownia_ISI_ZAF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurtowniaIsiZafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HurtowniaIsiZafApplication.class, args);
+	}
+
+}
